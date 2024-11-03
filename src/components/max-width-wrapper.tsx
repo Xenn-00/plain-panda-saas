90b5b68da268570407f199c517e4c50ba3917949
Xenn-00 <node.xenon@gmail.com>
2024-11-03 21:40:51 +0700

    feat: init

---
+++ src/components/max-width-wrapper.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils"
+import React from "react"
+
+type MaxWidthWrapperProps = {
+  children: React.ReactNode
+  className?: string
+}
+
+export const MaxWidthWrapper = ({
+  children,
+  className,
+}: MaxWidthWrapperProps) => {
+  return (
+    <div
+      className={cn(
+        "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
